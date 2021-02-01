8032606e79b7d555df36037b44a3e921afa32630
Rick Busarow <rickbusarow@gmail.com>
2021-02-01 17:57:31 -0600

    WIP

--- plugin/src/test/kotlin/com/rickbusarow/modulecheck/_kotestConfig.kt
+++ plugin/src/test/kotlin/com/rickbusarow/modulecheck/_kotestConfig.kt
@@ -1,7 +1,5 @@
-@file:Suppress("MatchingDeclarationName")
-
 /*
- * Copyright (C) 2020 Rick Busarow
+ * Copyright (C) 2021 Rick Busarow
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,6 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+@file:Suppress("MatchingDeclarationName")
 
 package com.rickbusarow.modulecheck
 
