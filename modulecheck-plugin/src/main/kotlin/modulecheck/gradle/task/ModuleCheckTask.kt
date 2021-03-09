89b6f434bdc6781fef9016014c24dbb8b0c92499
Rick Busarow <rickbusarow@gmail.com>
2021-03-09 11:50:03 -0600

    remove ANSI colors in logging

--- modulecheck-plugin/src/main/kotlin/modulecheck/gradle/task/ModuleCheckTask.kt
+++ modulecheck-plugin/src/main/kotlin/modulecheck/gradle/task/ModuleCheckTask.kt
@@ -21,7 +21,6 @@ import modulecheck.api.Project2
 import modulecheck.api.ProjectsAware
 import modulecheck.gradle.GradleProjectProvider
 import modulecheck.gradle.ModuleCheckExtension
-import modulecheck.gradle.internal.Output
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
@@ -50,6 +49,9 @@ abstract class ModuleCheckTask :
   @get:Input
   val projectProvider = GradleProjectProvider(project.rootProject, projectCache)
 
+  @get:Input
+  val logger = GradleLogger(project)
+
   @TaskAction
   fun evaluate() {
     val numIssues = measured {
@@ -73,25 +75,25 @@ abstract class ModuleCheckTask :
   private fun Collection<Finding>.finish(): Int {
     val grouped = this.groupBy { it.dependentPath }
 
-    Output.printMagenta("ModuleCheck found ${this.size} issues\n")
+    logger.printFailureHeader("ModuleCheck found ${this.size} issues\n")
 
     val unFixed = grouped
       .entries
       .sortedBy { it.key }
       .flatMap { (path, list) ->
 
-        Output.printMagenta("\t$path")
+        logger.printHeader("\t$path")
 
         val (fixed, toFix) = list.partition { finding ->
           autoCorrect && (finding as? Fixable)?.fix() ?: false
         }
 
         fixed.forEach { finding ->
-          Output.printYellow("\t\t${finding.logString()}")
+          logger.printWarning("\t\t${finding.logString()}")
         }
 
         toFix.forEach { finding ->
-          Output.printRed("\t\t${finding.logString()}")
+          logger.printFailure("\t\t${finding.logString()}")
         }
 
         toFix
@@ -110,7 +112,7 @@ abstract class ModuleCheckTask :
     @Suppress("MagicNumber")
     val secondsDouble = time / 1000.0
 
-    Output.printGreen("total parsing time: $secondsDouble seconds")
+    logger.printSuccessHeader("total parsing time: $secondsDouble seconds")
 
     return r
   }
