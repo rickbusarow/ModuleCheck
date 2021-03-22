fb05dcaf3cb4577f6e95f98d0ba637b7e62d7fe5
Rick Busarow <rickbusarow@gmail.com>
2021-03-21 22:13:02 -0500

    check all inherited configurations when checking whether a config is used

---
+++ modulecheck-core/src/main/kotlin/modulecheck/core/internal/uses.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 Rick Busarow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package modulecheck.core.internal
+
+import modulecheck.api.AndroidProject2
+import modulecheck.api.Config
+import modulecheck.api.ConfiguredProjectDependency
+import modulecheck.api.Project2
+import modulecheck.api.context.Declarations
+import modulecheck.api.context.importsForSourceSetName
+import modulecheck.api.context.possibleReferencesForSourceSetName
+import modulecheck.core.parser.android.androidResourceDeclarationsForSourceSetName
+
+fun Project2.uses(dependency: ConfiguredProjectDependency): Boolean {
+  val config = configurations[dependency.configurationName] ?: return false
+
+  val all = config.inherited + config
+
+  val depProject = dependency.project
+
+  return all.any { usesInConfig(it, depProject) }
+}
+
+fun Project2.usesInConfig(config: Config, depProject: Project2): Boolean {
+  val dependencyDeclarations = depProject[Declarations][config.name.asSourceSetName()]
+    .orEmpty()
+
+  val javaIsUsed = dependencyDeclarations
+    .any { declaration ->
+
+      declaration in importsForSourceSetName(config.name.asSourceSetName()) ||
+        declaration in possibleReferencesForSourceSetName(config.name.asSourceSetName())
+    }
+
+  if (javaIsUsed) return true
+
+  if (this !is AndroidProject2) return false
+
+  val rReferences =
+    possibleReferencesForSourceSetName(config.name.asSourceSetName())
+      .filter { it.startsWith("R.") }
+
+  val dependencyAsAndroid = depProject as? AndroidProject2 ?: return false
+
+  val resourcesAreUsed = dependencyAsAndroid
+    .androidResourceDeclarationsForSourceSetName(config.name.asSourceSetName())
+    .any { rDeclaration ->
+      rDeclaration in rReferences
+    }
+
+  return resourcesAreUsed
+}
