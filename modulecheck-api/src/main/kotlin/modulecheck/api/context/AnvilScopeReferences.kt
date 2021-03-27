a480972b1c276a0f8204f2355703d75b9ae48631
Rick Busarow <rickbusarow@gmail.com>
2021-03-26 19:48:57 -0500

    WIP

--- modulecheck-api/src/main/kotlin/modulecheck/api/context/AnvilScopeReferences.kt
+++ modulecheck-api/src/main/kotlin/modulecheck/api/context/AnvilScopeReferences.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2021 Rick Busarow
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package modulecheck.api.context
 
 import modulecheck.api.ConfigurationName
@@ -22,16 +7,16 @@ import modulecheck.api.SourceSetName
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 
-data class ResolvedReferences(
+data class AnvilScopeReferences(
   internal val delegate: ConcurrentMap<ConfigurationName, Set<ConfiguredProjectDependency>>
 ) : ConcurrentMap<ConfigurationName, Set<ConfiguredProjectDependency>> by delegate,
     ProjectContext.Element {
 
-  override val key: ProjectContext.Key<ResolvedReferences>
+  override val key: ProjectContext.Key<AnvilScopeReferences>
     get() = Key
 
-  companion object Key : ProjectContext.Key<ResolvedReferences> {
-    override operator fun invoke(project: Project2): ResolvedReferences {
+  companion object Key : ProjectContext.Key<AnvilScopeReferences> {
+    override operator fun invoke(project: Project2): AnvilScopeReferences {
       val map = project
         .configurations
         .mapValues { (configurationName, _) ->
@@ -58,8 +43,7 @@ data class ResolvedReferences(
             .toSet()
         }
 
-      return ResolvedReferences(ConcurrentHashMap(map))
+      return AnvilScopeReferences(ConcurrentHashMap(map))
     }
   }
 }
-
