c84a1f5e7e168630e7aba06741fb479fbfc87991
RBusarow <RBusarow@users.noreply.github.com>
2021-12-20 07:20:54 +0000

    sync dependency definitions
    Signed-off-by: GitHub Actions <actions@github.com>

--- dependabot-bridge/build.gradle.kts
+++ dependabot-bridge/build.gradle.kts
@@ -55,8 +55,8 @@ dependencies {
 
   dependencySync("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
   dependencySync("io.kotest:kotest-assertions-core-jvm:5.0.3")
-  dependencySync("io.kotest:kotest-property-jvm:5.0.1")
-  dependencySync("io.kotest:kotest-runner-junit5-jvm:5.0.1")
+  dependencySync("io.kotest:kotest-property-jvm:5.0.3")
+  dependencySync("io.kotest:kotest-runner-junit5-jvm:5.0.3")
 
   dependencySync("javax.inject:javax.inject:1")
 
