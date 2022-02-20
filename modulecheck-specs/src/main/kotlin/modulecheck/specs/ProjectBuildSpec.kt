00bf1395eda8cad8358d6244dad0b619e4414e58
Rick Busarow <RBusarow@users.noreply.github.com>
2022-02-20 16:22:51 -0600

    update AGP to 7.1.1 (#390)
    * update AGP to 7.1.1
    
    * sync dependency definitions
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * exclude AGP 7.1.1 with Gradle 7.1 in CI
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ProjectBuildSpec.kt
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- modulecheck-specs/src/main/kotlin/modulecheck/specs/ProjectBuildSpec.kt
+++ modulecheck-specs/src/main/kotlin/modulecheck/specs/ProjectBuildSpec.kt
@@ -18,7 +18,7 @@ package modulecheck.specs
 import java.nio.file.Path
 
 public val DEFAULT_GRADLE_VERSION: String = System
-  .getProperty("modulecheck.gradleVersion", "7.3.2")
+  .getProperty("modulecheck.gradleVersion", "7.4")
   /*
   * The GitHub Actions test matrix parses "7.0" into an Int and passes in a command line argument of "7".
   * That version doesn't resolve.  So if the String doesn't contain a period, just append ".0"
@@ -29,7 +29,7 @@ public val DEFAULT_GRADLE_VERSION: String = System
 public val DEFAULT_KOTLIN_VERSION: String =
   System.getProperty("modulecheck.kotlinVersion", "1.6.10")
 public val DEFAULT_AGP_VERSION: String =
-  System.getProperty("modulecheck.agpVersion", "7.0.3")
+  System.getProperty("modulecheck.agpVersion", "7.1.1")
 public val DEFAULT_ANVIL_VERSION: String =
   System.getProperty("modulecheck.anvilVersion", "2.3.11-1-6-10")
 
