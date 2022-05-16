6304a9b2406894018e7205036ec02bb1a9d5d4ad
Rick Busarow <rickbusarow@gmail.com>
2022-05-16 10:14:45 -0500

    require comments for public APIs in Detekt, and add baseline

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,10 +57,10 @@ moduleCheck {
   }
 }
 
-tasks.named("ktlintFormat") {
+tasks.matching { it.name == "ktlintFormat" }.configureEach {
   dependsOn(gradle.includedBuild("build-logic").task(":ktlintFormat"))
 }
-tasks.named("ktlintCheck") {
+tasks.matching { it.name == "ktlintCheck" }.configureEach {
   dependsOn(gradle.includedBuild("build-logic").task(":ktlintCheck"))
 }
 tasks.withType<Delete> {
@@ -68,4 +68,17 @@ tasks.withType<Delete> {
 }
 doctor {
   disallowCleanTaskDependencies.set(false)
+}
+
+val detektProjectBaseline by tasks.registering(io.gitlab.arturbosch.detekt.DetektCreateBaselineTask::class) {
+  description = "Overrides current baseline."
+  buildUponDefaultConfig.set(true)
+  ignoreFailures.set(true)
+  parallel.set(true)
+  setSource(files(rootDir))
+  config.setFrom(files("$rootDir/detekt/detekt-config.yml"))
+  baseline.set(file("$rootDir/detekt/detekt-baseline.xml"))
+
+  include("**/*.kt", "**/*.kts")
+  exclude("**/resources/**", "**/build/**", "**/src/test/java**", "**/src/test/kotlin**")
 }
