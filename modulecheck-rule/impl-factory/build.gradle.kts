6ea2736074a858186cc5c5a4a20ddaaeafa35491
RBusarow <RBusarow@users.noreply.github.com>
2022-07-28 15:51:06 +0000

    Apply ModuleCheck fixes
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modulecheck-rule/impl-factory/build.gradle.kts
+++ modulecheck-rule/impl-factory/build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
   api(libs.kotlinx.coroutines.core)
   api(libs.kotlinx.coroutines.jvm)
 
-  api(project(path = ":modulecheck-config:api"))
   api(project(path = ":modulecheck-dagger"))
   api(project(path = ":modulecheck-finding:api"))
   api(project(path = ":modulecheck-project:api"))
