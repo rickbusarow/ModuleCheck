0200dba5c78436b0ad485f08da5db15486b77bbc
Rick Busarow <rickbusarow@gmail.com>
2022-09-21 06:10:01 -0500

    add `NoUselessConstructorKeywordRule` and `NoTrailingSpacesInRawStringLiteralRule`

--- build-logic/ktlint-rules/src/test/kotlin/modulecheck/builds/ktlint/utils.kt
+++ build-logic/ktlint-rules/src/test/kotlin/modulecheck/builds/ktlint/utils.kt
@@ -15,26 +15,22 @@
 
 package modulecheck.builds.ktlint
 
-import com.google.auto.service.AutoService
+import com.pinterest.ktlint.core.LintError
 import com.pinterest.ktlint.core.RuleProvider
-import com.pinterest.ktlint.core.RuleSetProviderV2
+import com.pinterest.ktlint.core.api.EditorConfigOverride
+import com.pinterest.ktlint.test.format as ktlintTestFormat
 
-/**
- * TODO
- *
- * @since 0.13.0
- */
-@AutoService(RuleSetProviderV2::class)
-class McRuleSetProvider : RuleSetProviderV2(
-  id = "mc-rules",
-  about = NO_ABOUT
-) {
-
-  override fun getRuleProviders(): Set<RuleProvider> {
-    return setOf(
-      RuleProvider { NoSinceInKDocRule() },
-      RuleProvider { NoTrailingSpacesInRawStringLiteralRule() },
-      RuleProvider { NoUselessConstructorKeywordRule() }
-    )
-  }
-}
+fun Set<RuleProvider>.format(
+  text: String,
+  editorConfigOverride: EditorConfigOverride = EditorConfigOverride.emptyEditorConfigOverride,
+  userData: Map<String, String> = emptyMap(),
+  cb: (e: LintError, corrected: Boolean) -> Unit = { _, _ -> },
+  script: Boolean = false
+): String = ktlintTestFormat(
+  lintedFilePath = null,
+  text = text,
+  editorConfigOverride = editorConfigOverride,
+  userData = userData,
+  cb = cb,
+  script = script
+)
