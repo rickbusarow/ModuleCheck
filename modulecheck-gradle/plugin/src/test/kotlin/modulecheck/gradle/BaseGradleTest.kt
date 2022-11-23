0fa9f1333fa588a8946a5f7c0021f874b1fcffba
Rick Busarow <rickbusarow@gmail.com>
2022-11-22 20:33:15 -0600

    add `detektAll` task and add `detektTest` to CI

--- modulecheck-gradle/plugin/src/test/kotlin/modulecheck/gradle/BaseGradleTest.kt
+++ modulecheck-gradle/plugin/src/test/kotlin/modulecheck/gradle/BaseGradleTest.kt
@@ -38,7 +38,7 @@ import org.junit.jupiter.api.DynamicTest
 import java.io.File
 import kotlin.text.RegexOption.IGNORE_CASE
 
-@Suppress("PropertyName")
+@Suppress("PropertyName", "UnnecessaryAbstractClass")
 abstract class BaseGradleTest :
   BaseTest(),
   ProjectCollector,
@@ -136,7 +136,7 @@ abstract class BaseGradleTest :
     root
   }
 
-  val gradleRunner by resets {
+  val gradleRunner: GradleRunner by resets {
     GradleRunner.create()
       .forwardOutput()
       .withGradleVersion(gradleVersion)
