e40f5aff8b794e2d9babc804b356a3b85e056df6
Rick Busarow <rickbusarow@gmail.com>
2022-11-25 09:38:25 -0600

    update versions matrix for Gradle 7.6

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -13,27 +13,12 @@
  * limitations under the License.
  */
 
-import org.jmailen.gradle.kotlinter.KotlinterExtension
-
-/*
- * Copyright (C) 2021-2022 Rick Busarow
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 buildscript {
   dependencies {
-    classpath(libs.kotlin.gradle.plug)
-    classpath(libs.vanniktech.publish)
+    // Gradle 7.6 has a dependency resolution bug which tries to use Kotlin 1.7.10
+    // for transitive dependencies like `sam-with-receiver`.
+    // https://github.com/gradle/gradle/issues/22510
+    classpath(libs.kotlin.sam.with.receiver)
   }
 }
 
@@ -65,7 +50,7 @@ allprojects {
   }
 
   afterEvaluate {
-    configure<KotlinterExtension> {
+    configure<org.jmailen.gradle.kotlinter.KotlinterExtension> {
       ignoreFailures = false
       reporters = arrayOf("checkstyle", "plain")
       experimentalRules = true
