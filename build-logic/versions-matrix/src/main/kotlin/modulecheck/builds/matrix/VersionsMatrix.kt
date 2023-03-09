d415c45e74a6cc74975e0d03dd4a418544ec0985
Rick Busarow <rickbusarow@gmail.com>
2023-03-09 04:25:44 -0600

    drop kotlin 1.7.x testing

--- build-logic/versions-matrix/src/main/kotlin/modulecheck/builds/matrix/VersionsMatrix.kt
+++ build-logic/versions-matrix/src/main/kotlin/modulecheck/builds/matrix/VersionsMatrix.kt
@@ -24,32 +24,15 @@ class VersionsMatrix(
 ) {
 
   val agpList = agpArg?.singletonList()
-    ?: listOf("7.1.3", "7.2.2", "7.3.1", "7.4.2")
+    ?: listOf("7.3.1", "7.4.2")
   val anvilList = anvilArg?.singletonList()
-    ?: listOf("2.4.3", "2.4.4")
+    ?: listOf("2.4.4")
   val gradleList = gradleArg?.singletonList()
     ?: listOf("7.5.1", "7.6.1", "8.0.2")
   val kotlinList = kotlinArg?.singletonList()
-    ?: listOf("1.7.22", "1.8.0", "1.8.10")
+    ?: listOf("1.8.0", "1.8.10", "1.8.20-RC")
 
-  internal val exclusions = listOf(
-    Exclusion(gradle = "7.5.1", agp = "7.3.1", anvil = null, kotlin = null),
-    Exclusion(gradle = null, agp = "7.0.1", anvil = "2.4.4", kotlin = null),
-    Exclusion(gradle = "7.5.1", agp = null, anvil = null, kotlin = "1.8.10"),
-    Exclusion(gradle = "7.5.1", agp = "7.4.2", anvil = null, kotlin = null),
-    Exclusion(gradle = "8.0.2", agp = "7.1.3", anvil = null, kotlin = null),
-    Exclusion(gradle = "8.0.2", agp = "7.2.2", anvil = null, kotlin = null),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.3", kotlin = "1.8.0"),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.3", kotlin = "1.8.10"),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.4", kotlin = "1.7.22"),
-    Exclusion(gradle = "7.4.2", agp = null, anvil = "2.4.4", kotlin = null),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.3", kotlin = "1.8.0"),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.3", kotlin = "1.8.10"),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.4", kotlin = "1.7.0"),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.4", kotlin = "1.7.10"),
-    Exclusion(gradle = null, agp = null, anvil = "2.4.4", kotlin = "1.7.22")
-  )
-    .requireNoDuplicates()
+  internal val exclusions = listOf<Exclusion>().requireNoDuplicates()
 
   private val latest by lazy { allValid.last() }
 
