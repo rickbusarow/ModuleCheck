9366dbd3e7bb22f7e17c3e5b47999b2f0787a463
Rick Busarow <rickbusarow@gmail.com>
2023-04-26 14:14:31 -0500

    Merge branch 'main' into McElement_WIP

--- build-logic/versions-matrix/src/main/kotlin/modulecheck/builds/matrix/VersionsMatrix.kt
+++ build-logic/versions-matrix/src/main/kotlin/modulecheck/builds/matrix/VersionsMatrix.kt
@@ -23,7 +23,7 @@ class VersionsMatrix(
   private val kotlinArg: String? = null
 ) {
 
-  internal val gradleListDefault = listOf("7.5.1", "7.6.1", "8.0.2", "8.1")
+  internal val gradleListDefault = listOf("7.5.1", "7.6.1", "8.0.2", "8.1.1")
   internal val agpListDefault = listOf("7.3.1", "7.4.2", "8.0.0")
   internal val anvilListDefault = listOf("2.4.5")
   internal val kotlinListDefault = listOf("1.8.0", "1.8.10", "1.8.20")
@@ -34,8 +34,8 @@ class VersionsMatrix(
   val kotlinList = kotlinArg?.singletonList() ?: kotlinListDefault
 
   internal val exclusions = listOf<Exclusion>(
-    Exclusion(gradle = "8.1", agp = "7.3.1", anvil = null, kotlin = null),
-    Exclusion(gradle = "8.1", agp = "7.4.2", anvil = null, kotlin = null),
+    Exclusion(gradle = "8.1.1", agp = "7.3.1", anvil = null, kotlin = null),
+    Exclusion(gradle = "8.1.1", agp = "7.4.2", anvil = null, kotlin = null),
     Exclusion(gradle = "7.5.1", agp = "8.0.0", anvil = null, kotlin = null),
     Exclusion(gradle = "7.6.1", agp = "8.0.0", anvil = null, kotlin = null),
   ).requireNoDuplicates()
