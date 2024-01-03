bc98529d400f8458b5d95561216371972f0efc07
Rick Busarow <rickbusarow@gmail.com>
2024-01-03 17:35:18 -0600

    update version matrix for Gradle 8.5

--- build-logic/versions-matrix/src/main/kotlin/modulecheck/builds/matrix/VersionsMatrix.kt
+++ build-logic/versions-matrix/src/main/kotlin/modulecheck/builds/matrix/VersionsMatrix.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2023 Rick Busarow
+ * Copyright (C) 2021-2024 Rick Busarow
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,8 +23,8 @@ class VersionsMatrix(
   private val kotlinArg: String? = null
 ) {
 
-  internal val gradleListDefault = listOf("7.6.1", "8.0.2", "8.1.1")
-  internal val agpListDefault = listOf("7.3.1", "8.0.2", "8.1.0")
+  internal val gradleListDefault = listOf("8.0.2", "8.1.1", "8.4", "8.5")
+  internal val agpListDefault = listOf("8.0.2", "8.1.0")
   internal val anvilListDefault = listOf("2.4.6")
   internal val kotlinListDefault = listOf("1.8.0", "1.8.10", "1.8.22")
 
@@ -33,11 +33,7 @@ class VersionsMatrix(
   val anvilList = anvilArg?.singletonList() ?: anvilListDefault
   val kotlinList = kotlinArg?.singletonList() ?: kotlinListDefault
 
-  internal val exclusions = listOf<Exclusion>(
-    Exclusion(gradle = "8.1.1", agp = "7.3.1", anvil = null, kotlin = null),
-    Exclusion(gradle = "7.6.1", agp = "8.0.2", anvil = null, kotlin = null),
-    Exclusion(gradle = "7.6.1", agp = "8.1.0", anvil = null, kotlin = null)
-  ).requireNoDuplicates()
+  internal val exclusions = listOf<Exclusion>().requireNoDuplicates()
 
   private val latest by lazy { allValid.last() }
 
